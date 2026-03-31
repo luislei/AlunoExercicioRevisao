09872daa12a5aff2afdad4e8c55f70171b9ea7f4
luislei <luismlei@gmail.com>
2026-03-31 20:09:47 +0100

    inicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FundamentosAlgoritmia;
+ 
+public class Program
+
+{
+static void Main(string[] args)
+{
+    Console.WriteLine("Exercicios Revisão \n");
+   
+    AlunoExercicioRevisao alunoExercicioRevisao = new AlunoExercicioRevisao();
+    alunoExercicioRevisao.ExecutarExercicioRevisao();
+    
+}
+
+}   // fim do public class 
+
+// Fim do namespace
